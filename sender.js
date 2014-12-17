db3fadfb9e1a40c957a87a8fa3f99bc3e59d9c14
spencerandhill <jan.rathmer@gmail.com>
2014-12-17 23:21:07 +0100

    Funktionen zum laden('read(datapath, callback)') und abspielen('send(callback)') der Daten hinzugefügt

--- sender.js
+++ sender.js
@@ -3,31 +3,67 @@ var HOST = '192.168.1.61';
 
 var dgram = require('dgram');
 var pcapp = require('pcap-parser');
+var util = require('util');
 
+var datapath = 'record.pcap';
 var socket = dgram.createSocket('udp4');
-var parser = new pcapp.parse('record.pcap');
 var packages = [];   //This the Container for the packages
+var containerLength=0;
 
-
-parser.on('packetData', function(package){
-    packages.push(package); //Fill Container with packages
-});
-
-parser.on('end', function(){
-    if(packages.length > 0)
-        send(packages.shift()); //Get first element and send it out
-});
-
-function send (package){
-    socket.send(package, 0, package.length, PORT, HOST, function(err, bytes) {
-        if (err) throw err;
-        console.log('UDP message sent to ' + HOST +':'+ PORT);
-        if(packages.length > 0)
-            send(packages.shift());   //Send next package
+read(datapath, function(length){
+    console.log("DataRead successfull\tPackageCount: " + length);
+    send(function(result){
+        if(result=="ok")
+            console.log("Data successfull sent!");
         else
-        {
-            socket.close();
-            console.log("socket closed!");
-        }
-    });  
+            console.log("Error occurs: " + result);
+    });
+});
+
+function sendPackage (package, callback){
+    if(package)
+    {
+        console.log("[" + (containerLength - packages.length) + "][" + containerLength + "]" + " sent to " + HOST + ":" + PORT);
+        socket.send(package, 0, package.length, PORT, HOST, function(err, bytes) {
+            if (err) 
+            {
+                callback(err);
+                throw err;
+            }
+
+            if(packages.length > 0)
+            {
+                sendPackage(packages.shift(),function(result){ //Send next package recursive
+                    callback(result)    //pass result to callback
+                });
+            }
+            else
+            {
+                callback("ok");
+                socket.close();
+                console.log("Socket closed");
+            }
+        });
+    }
+    else
+        callback("There's no Data\nPlease read in Data first");
+}
+
+function send(callback){
+    sendPackage(packages.shift(), function(result){
+        callback(result);
+    });
+}
+
+function read(datapath, callback){
+    var parser = new pcapp.parse(datapath);
+ 
+    parser.on('packetData', function(package){
+        packages.push(package); //Fill Container with packages
+    });
+    
+    parser.on('end', function(){
+        containerLength = packages.length;
+        callback(containerLength);
+    });
 }
