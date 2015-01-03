57b568dba11e50a1de958e82658e85aae25beb6c
spencerandhill <jan.rathmer@gmail.com>
2015-01-03 20:56:37 +0100

    Start of AngularJS Webapp

---
+++ AngularJS/PiCube/app.js
@@ -0,0 +1,50 @@
+/**
+ * Created by jan on 29.12.14.
+ */
+angular.module('picube', [])     //Name des Moduls
+    .factory('fileFactory', function($http) {          //Name der Factory
+        var files = [];
+
+        return {
+            getFiles: function(callback) {
+                $http.get('files.json').then(function (Response) {   //zieht alle aktuellen Files per REST vom Server (im Moment noch aus files.json)
+                    files = Response.data;
+                    callback(files);
+                });
+            },
+
+            postFile: function(filepath){    //fügt ein File zum Server hinzu
+                $http.post("localhost/addFile", filepath).then(function (Response) {   //schiebt ein neues File zum Server
+                    files = Response.data;
+                });
+                return files;
+                console.log("Post ausgelöst");
+            },
+
+            playFile: function(filepath){
+
+                console.log("Play ausgelöst");
+                console.log(filepath);
+            },
+
+            removeFile: function(filepath){ //löscht ein File vom Server
+                $http.delete("http://localhost/addFile", filepath).then(function(callback){
+                    console.log(callback);
+                });
+
+                console.log("Remove ausgelöst");
+                console.log(filepath);
+            }
+        };
+    })
+    .controller('FileCtrl', function($scope, fileFactory) {
+        $scope.fileFactory = fileFactory;
+
+        init();
+
+        function init() {
+            fileFactory.getFiles(function(files){
+                $scope.files = files;
+            });
+        }
+    });
