6bd4895b8067bbfadcdedf9058cb65dd50381827
spencerandhill <jan.rathmer@gmail.com>
2015-01-08 03:07:23 +0100

    mongodb-dependencie removed. everything is generated locally now. more efficient

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 // set up ========================
     var express  = require('express');
     var server = express();                     // create our server w/ express
-    var multer = require('multer');
-    var mongoose = require('mongoose');                     // mongoose for mongodb-connection
+    var multer = require('multer');             // used for File-Upload
     var morgan = require('morgan');             // log requests to the console (express4)
     var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var walk    = require('walk');              // Used to walk recursive through folders
+    var fs = require('fs');                     // Used for filesystem-access
+    var uuid = require('node-uuid');            // used to generate uuid's to identify files
+
+    var uploadFolder = 'static/uploads';        //Define Upload-Folder for all Actions with Files
+    var files   = [];                           //Stores file-items
 
     // configuration =================
-
-    mongoose.connect('mongodb://localhost:27017/');     // connect to local running mongoDB database on localhost:27017
 
     server.use(morgan('dev'));                                         // log every request to the console
     server.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
     server.use(bodyParser.json());                                     // parse application/json
     server.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
     server.use(express.static(__dirname + '/static'));
-    
+
     //Enable CORS to solve the same-origin-policy problem
     server.use(function(req, res, next) {
         res.header('Access-Control-Allow-Origin', '*');
@@ -31,76 +34,123 @@
         }
     });
 
-// define model =================
-    var dbFiles = mongoose.model('Files', {
-        name : String,
-        path : String
-    });
-
 //File-Upload
     server.use(multer({
-        dest: './static/uploads/',      //Output-Folder of Files
+        dest: uploadFolder,      //Output-Folder of Files
         rename: function (fieldname, filename) {
+            //\W+/g = findet alle Zeichen, die nicht alphanumerisch und auch keine Unterstriche sind
             return filename.replace(/\W+/g, '-').toLowerCase();
         }
     }));
+
+//Functions
+
+    //checks, if every file in files[] is still available on the server
+    //if not, it is removed from files[]
+    var checkFilesExistence = function() {
+        var index = 0;
+        files.forEach(function(item) {
+            //check if file is still available on server
+            fs.exists(item['path'], function(exists) {
+                if (!exists) {
+                    files = files.splice(index,1);      //remove this specific item from files[]
+                }
+            });
+            index++;
+        });
+    }
+
+    //checks, if a File is already in files[] or not
+    //if not false is returned, otherwise true
+    var FileLookup = function(filepath, callback) {
+        var index = -1;
+        files.forEach(function(item) {
+            if(item['path'] === filepath) {
+                index = 1;
+            }
+        });
+        callback(index);
+    }
+
+    //gets the filepath related to a id from files[]
+    var getFilePath = function(fileid, callback) {
+        var output = files.filter(function(item){
+            return item.id==fileid});
+
+        callback(output[0].path);       //output should contain just one item, with the right id
+    }
+
+    //checks the complete content of uploadFolder
+    //if a file isn't in files[] it is added to files[]
+    //checks also, if every file in files[] is still available
+    var getFolderContent = function(callback) {
+        checkFilesExistence();  //check if every file in files[] is still available
+// Walker options
+     var walker  = walk.walk(uploadFolder, { followLinks: false });
+
+        walker.on('file', function(root, file, next) {
+            var filename = file.name.split('.'); //generate name from path
+            filename = filename[filename.length - 2] + "." + filename[filename.length -1];
+            //       = name.fileExtension       e.g. record.pcap
+
+            if(file.name.split('.')[file.name.split('.').length - 1] == "pcap") {   //Just take Files, which are pcap-Files
+                fs.stat((root + '/' + file.name), function (err, stats) {
+                    if (err) {
+                        console.log(err);
+                        next();
+                    }
+                    else {
+                        FileLookup(root + '/' + file.name, function(result) {   //check if this file is already known
+                            if(result === -1) { //if result === -1 the file is not in files[]
+                                var fileid = uuid.v4(); //generate an id for the file
+                                var filesizeinMB = Number((stats["size"] / 1000000.0).toFixed(2));  //get the filesize from stats['size']
+
+                                files.push({    //add this file to files[]
+                                    id: fileid,
+                                    name: filename,
+                                    path: root + '/' + file.name,
+                                    sizeinMB: filesizeinMB
+                                });
+                                next();
+                            }
+                            else
+                                next();
+                        });
+                    }
+                });
+            }
+            else
+                next();
+        });
+
+        walker.on('end', function() {
+            callback(files);
+        });
+    }
+
+    var removeFile = function(filePath, callback) {
+        fs.unlink(filePath, function(err) {});
+    }
 
 // routes ======================================================================
 
     // api ---------------------------------------------------------------------
     // get all files
     server.get('/api/files', function(req, res, next) {
-
-        // use mongoose to get all files in the database
-        dbFiles.find(function(err, files) {
-
-            // if there is an error retrieving, send the error. nothing after res.send(err) will execute
-            if (err)
-                res.send(err)
-
-            res.json(files); // return all files in JSON format
+        getFolderContent(function(files) {
+            res.json(files)
         });
-    });
-
-    // create file and send back all files after creation
-    server.post('/api/files', function(req, res, next) {
-        // create a file, information comes from AJAX request from Angular
-        var filepath = req.body.path;
-        var filename = filepath.split('.')[filepath.split('.').length - 2]; //generate name from path
-        
-        dbFiles.create({
-            name : filename,
-            path : filepath
-        }, function(err, files) {
-            if (err)
-                res.send(err);
-
-            // get and return all the files after you create another
-            dbFiles.find(function(err, files) {
-                if (err)
-                    res.send(err)
-                res.json(files);
-            });
-        });
-
     });
 
     // delete a file
     server.delete('/api/files/:file_id', function(req, res, next) {
-       dbFiles.remove({
-            _id : req.params.file_id
-        }, function(err, files) {
-            console.log(err);
-            if (err)
-                res.send(err);
 
-            // get and return all the files after you create another
-            dbFiles.find(function(err, files) {
-                if (err)
-                    res.send(err)
+        getFilePath(req.params.file_id, function(filepath) {
+            removeFile(filepath);
+            getFolderContent(function(files) {
                 res.json(files);
-            });
-        });
+            })
+        })
     });
 
     // upload a file
--- static/PiCube/PiCube/app.js
+++ static/PiCube/PiCube/app.js
@@ -30,19 +30,13 @@ angular.module('picube', ['angularFileUpload', 'ngRoute'])
                 return files;
             },
 
-            postFile: function(filepath){
-                $http.post("http://localhost:8080/api/files/", filepath).then(function (Response) {   //adds a new file to the server-api, tested with postman
-                    angular.copy(Response.data, files);  //Update Data
-                });
-            },
-
             playFile: function(filepath){   //this is not important at the moment
                 console.log("Play called");
                 console.log(filepath);
             },
 
-            removeFile: function(_id){ //deletes a file from server, tested with postman
-                $http.delete("http://localhost:8080/api/files/" + _id).then(function(Response){
+            removeFile: function(fileid){ //deletes a file from server, tested with postman
+                $http.delete("http://localhost:8080/api/files/" + fileid).then(function(Response){
                     angular.copy(Response.data, files);  //Update Data
                 });
             }
